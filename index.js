efd4a1d90af7f5916fb543174676784f6bef7ad5
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-17 07:08:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 18;
+let num4 = 7;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
