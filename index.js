4ce0c281f85e851e4060f281314d2daf6d016d81
eman hamid <emanhharki@gmail.com>
2021-04-25 17:09:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+// let num1 = 2;
+// let num2 = 31;
+// let result = num1 * num2;
+// console.log(result);
+
+
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+
+let random=90;
+if (random) {
+console.log(Math.random());
+}else{
+  console.log("no random");
+}
+
+
+
+let num3 = 14;
+let num4 =5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max=20;
+console.log(Math.max(1, 20, 2));
