884230bac4f5d5da40b68283e07d245637e8fa04
Hasar Ali <hasar.ali@protonmail.com>
2021-04-26 00:34:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31, 
+      num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+const num3 = 24,
+num4 = 5;
+let mod = num3%num4;
+
+const set = [20];
+const max = Math.max(set);
