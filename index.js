cbc5bf91c69487fe7820edf0acde0f3ea74fb522
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-13 17:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let num3 = 24;
+let num4 = 5;
+
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random()*100)+1;
+
+const mod = num3%num4;
+
+const max = Math.max(1, 3, 20);
