714fd3ea4854928dbadff1aa3b304cb0a511a9f3
Avraz  Zebary <avrazebary@gmail.com>
2021-05-14 15:22:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*1000) +1;
+
+const rem1 = 10;
+const rem2 = 6;
+const remainder = rem1%rem2;
+
+const max = Math.max(11, 12 , 13, 14, 15, 1, 4,  20 );
