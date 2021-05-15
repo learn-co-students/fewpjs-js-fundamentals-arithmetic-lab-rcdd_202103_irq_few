d81ef6be0024771ce8efb11d2697c1edd5d24b20
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-15 12:57:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+
+  let num1=2;
+  let num2=31;
+  let multiply = num1*num2;
+  console.log(multiply);
+  
+let random = Math.floor(Math.random()*8) +1 ;
+console.log(random);
+
+let num3=12;
+let num4=8; 
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max= Math.max(2,3,3,20);
