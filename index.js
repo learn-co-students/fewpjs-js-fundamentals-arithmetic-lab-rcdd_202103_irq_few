b500bc14b07b195e44f36834198121bdeb739fa2
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-15 10:32:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let num3 = 28;
+let num4 = 6;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(5,10,15,20);
