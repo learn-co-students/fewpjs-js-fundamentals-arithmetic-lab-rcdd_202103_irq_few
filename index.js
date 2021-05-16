71a6d6ec15d474c5c3b7c6b77fa23cf74ce89f35
Mariam  Khamo <mariamw736@gmail.com>
2021-05-16 21:07:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 11);
+console.log(random);
+
+var num3 = 19;
+var num4 = 5;
+var mod = num3 % num4;
+var max = Math.max(5, 16, 18, 20);
