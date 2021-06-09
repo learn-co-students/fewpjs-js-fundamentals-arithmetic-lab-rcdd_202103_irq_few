472c8c56904cf8856961dea2084a3458ccc9a2f9
Zhino Salam <zhinosalam188@gmail.com>
2021-06-09 09:06:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var num3 = 2;
+var num4 =  2;
+var multiply = num1*num2;
+var random = Math.floor(Math.random() * 10) + 1;
+var mod = 24%5;
+var max = Math.max(1,2,3,5,8,13,20);
