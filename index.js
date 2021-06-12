b98503a4a851e33fcb4926b3c5a827a6a3c5a705
Yusra Ahmed <isra.00287446@gmail.com>
2021-06-12 11:12:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code her
+let num1 = 31
+let num2 = 2
+let num3 = 7
+let num4 = 11
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num4 % num3;
+
+
+let max = Math.max(1,3,20);
